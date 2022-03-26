acfd336862e405219713752ad9d81e66988d42dd
Your Name <you@example.com>
2022-03-26 12:15:13 +0300

    Добавил обработку 404 и 401

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 import sqlite3
 
 app = Flask(__name__)
@@ -24,6 +24,9 @@ def about():
 
 @app.route("/profile/<username>")
 def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+
     return f"Пользователь: {username}"
 
 @app.route("/contact", methods=["POST", "GET"])
@@ -46,6 +49,23 @@ def contact():
 
     return render_template('contact.html', title = 'Обратная связь', menu=menu)
 
+@app.errorhandler(404)
+def pageNotFound(error):
+    return render_template('page404.html', title='404', menu=menu), 404
+
+@app.errorhandler(401)
+def pageNotYour(error):
+    return render_template('page401.html', title='401', menu=menu), 401
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == 'imichael' and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title='Авторизация')
 '''
 with app.test_request_context():
     print( url_for('about') )'''
